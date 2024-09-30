c4a51b2c31604367597d727134b18b227738b0fc
Robin Montferme <robin.montferme@gmail.com>
2024-09-30 23:58:46 +0200

    added more util functions simplified deserialization

--- classic_deserializer.py
+++ classic_deserializer.py
@@ -1,6 +1,6 @@
 
 from binascii import hexlify
-from read_bytes_utils import read_next, read_str
+from read_bytes_utils import read_next, read_str,read_short
 from classic_level_data import ClassicLevelData
 class ClassicDeserializer:
     """Class to deserialize minecraft classic level using 3rd format type. 
@@ -14,8 +14,12 @@ class ClassicDeserializer:
         obj_token = read_next(data,9,10)
         class_token = read_next(data,10,11)        
         class_name = read_str(data,11)
-        shift+= len(class_name)
+        shift+= len(class_name)+2
         print(class_name)
+        uuid = read_next(data,11+shift,19+shift)
+        class_handle = read_next(data,19+shift,20+shift)
+        nb_of_field = read_short(data,20+shift)
+        print(nb_of_field)
         
         
         deserialized_data = ClassicLevelData()
--- read_bytes_utils.py
+++ read_bytes_utils.py
@@ -1,4 +1,9 @@
 from binascii import hexlify
+BYTE_SIZE = 1
+SHORT_SIZE = 2
+INT_SIZE = 4
+LONG_SIZE = 8
+
 def print_hex_value(byte_stream: bytes):
     print(hexlify(byte_stream," ",1).decode())
 
@@ -7,9 +12,38 @@ def read_next(data: bytes,start:int,end:int):
     print_hex_value(read_data)
     return read_data
 
+def read_byte(data:bytes, start):
+    return read_next(data,start,start+BYTE_SIZE)
+
+def read_char(data:bytes, start:int):
+    return read_byte(data,start).decode("utf-8")
+
+def read_short(data,start):
+    return int.from_bytes(read_next(data,start,start+SHORT_SIZE))
+
+def read_int(data,start):
+    return int.from_bytes(read_next(data,start,start+INT_SIZE))
+
+def read_long(data,start):
+    return int.from_bytes(read_next(data,start,start+LONG_SIZE))
+
 def read_str(data:bytes, start:int):
-    length_bytes = read_next(data,start,start+2)
-    length = int.from_bytes(length_bytes)
-    string_bytes = read_next(data,start+2,start+2+length)
+    length = read_short(data,start)
+    str_start = start+SHORT_SIZE
+    str_end = str_start+length
+    string_bytes = read_next(data,str_start,str_end)
     return string_bytes.decode("utf-8")
+
+def match_type(type_token:str):
+    match(type_token):
+        case 'I':
+            return int
+        case 'B':
+            return bytes
+
+def read_field(data:bytes,start:int):
+    type_char = read_char(data,start)
+    field_name = read_str(data,start+BYTE_SIZE)
+
+
 
